3279b3ad8fd688ec760da21e771962cbcbc6b81b
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-03-17 07:30:12 -0600

    fixed my error can now post

--- elephantroomproject/elephantapp/views/topic/topiclist.py
+++ elephantroomproject/elephantapp/views/topic/topiclist.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 from django.shortcuts import reverse
 from django.shortcuts import redirect
 from elephantapp.models import Topic
+from elephantapp.models import Profile
+from elephantapp.models import Category
 from ..connection import Connection
 
 def topic_list(request):
     if request.method == 'GET':
+        current_user = request.user
+        current_profile_user = Profile.objects.get(user_id=current_user.id)
         with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = sqlite3.Row
             db_cursor = conn.cursor()
@@ -53,6 +57,8 @@ def topic_list(request):
         return render(request, template, context)
     
     elif request.method == 'POST':
+        current_user = request.user
+        current_profile_user = Profile.objects.get(user_id=current_user.id)
         form_data = request.POST
 
         with sqlite3.connect(Connection.db_path) as conn:
@@ -62,14 +68,14 @@ def topic_list(request):
             INSERT INTO elephantapp_topic
             (
                 title, stance_text_body, is_anecdote,
-                is_citable, blurb, user_id, resource_link,
+                is_citable, user_id, blurb, resource_link,
                 image_link, is_free_resource, is_proponent
             )
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             """,
             (form_data['title'], form_data['stance_text_body'],
                 form_data['is_anecdote'], form_data['is_citable'],
-                request.auth.user.id, form_data['blurb'], form_data['resource_link'],
+                current_user.id, form_data['blurb'], form_data['resource_link'],
                 form_data['image_link'], form_data['is_free_resource'], form_data['is_proponent']))
 
         return redirect(reverse('elephantapp:topics'))
